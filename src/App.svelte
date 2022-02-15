7f82b889c5f3cf657389e25228ee742295fb3c5a
Chris Repka <chris.repka@twdb.texas.gov>
2022-02-15 16:04:15 -0600

    Columns added, image placeholder. align-items center for vertical align

--- src/App.svelte
+++ src/App.svelte
@@ -10,57 +10,70 @@
 </script>
 
 <main>
-  <h1>TNRIS Store Updates</h1>
-  <p>
-    We are building a store! We don't know for certain what our offerings will
-    be, but we are doing research to find out what our users want. Subscribe to
-    our mailing list below to get updates on our progress.
-  </p>
-  <form on:submit|preventDefault={validateSubmission}>
-    <div class="inputsInline">
-      <label for="firstName">
-        First Name
-        <input
-          id="firstName"
-          type="text"
-          bind:value={firstName}
-          minlength="3"
-          maxlength="32"
-          pattern="[a-zA-Z-]*"
-          required
-        />
-      </label>
-
-      <label for="lastName">
-        Last Name
-        <input
-          id="lastName"
-          type="text"
-          bind:value={lastName}
-          minlength="3"
-          maxlength="32"
-          pattern="[a-zA-Z-]*"
-          required
-        />
-      </label>
+  <div id="left">
+    <div id="interestStatement">
+      <h1>TNRIS Store Updates</h1>
+      <p>
+        We are building a store! We don't know for certain what our offerings
+        will be, but we are doing research to find out what our users want.
+        Subscribe to our mailing list below to get updates on our progress.
+      </p>
     </div>
-    <label for="email">
-      Email
-      <input
-        id="email"
-        type="email"
-        bind:value={email}
-        minlength="4"
-        maxlength="64"
-        required
-      />
-    </label>
-    <label for="overThirteen">
-      <input type="checkbox" id="overThirteen" bind:checked={overThirteen} required pattern="true" />
-      I am over the age of thirteen.
-    </label>
-    <button>Submit</button>
-  </form>
+    <form on:submit|preventDefault={validateSubmission}>
+      <div class="inputsInline">
+        <label for="firstName">
+          First Name
+          <input
+            id="firstName"
+            type="text"
+            bind:value={firstName}
+            minlength="3"
+            maxlength="32"
+            pattern="[a-zA-Z-]*"
+            required
+          />
+        </label>
+
+        <label for="lastName">
+          Last Name
+          <input
+            id="lastName"
+            type="text"
+            bind:value={lastName}
+            minlength="3"
+            maxlength="32"
+            pattern="[a-zA-Z-]*"
+            required
+          />
+        </label>
+      </div>
+      <label for="email">
+        Email
+        <input
+          id="email"
+          type="email"
+          bind:value={email}
+          minlength="4"
+          maxlength="64"
+          required
+        />
+      </label>
+      <label for="overThirteen">
+        <input
+          type="checkbox"
+          id="overThirteen"
+          bind:checked={overThirteen}
+          required
+          pattern="true"
+        />
+        I am over the age of thirteen.
+      </label>
+      <button>Submit</button>
+    </form>
+  </div>
+  <div id="right">
+    <img id="image" src="#" />
+  </div>
 </main>
 
 <style lang="scss">
@@ -68,6 +81,20 @@
     padding: 1em;
     max-width: 100%;
     margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
+
+    #left,
+    #right {
+      border: solid 1px lightgrey;
+      border-radius: 8px;
+      padding: 16px;
+    }
+
+    #right {
+      background: lightgray;
+    }
   }
 
   h1 {
@@ -79,9 +106,6 @@
 
   form {
     padding: 16px;
-    border: solid 1px lightgrey;
-    border-radius: 8px;
-
     .inputsInline {
       display: inline-flex;
     }
@@ -117,7 +141,7 @@
 
   @media (min-width: 640px) {
     main {
-      max-width: none;
+      max-width: 1200px;
     }
   }
   @media (max-width: 639px) {
